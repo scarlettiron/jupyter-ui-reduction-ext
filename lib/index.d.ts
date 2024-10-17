87c4d2d1bf2376950be56c470ff64e6cd5af347b
scarlettiron <scottscarlett@gmail.com>
2024-10-16 23:41:19 -0400

    Newest version of jupyterlab

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+import { JupyterFrontEndPlugin } from '@jupyterlab/application';
+/**
+ * A JupyterLab plugin to completely remove the left sidebar.
+ */
+declare const extension: JupyterFrontEndPlugin<void>;
+export default extension;
